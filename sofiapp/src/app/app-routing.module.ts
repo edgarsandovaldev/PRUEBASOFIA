c2c0def388a8d5fb5be15ebfcdc06d1a60f2fbfa
Edgar Sandoval Desarrollador <59340481+edgarsandovaldev@users.noreply.github.com>
2022-10-21 22:43:51 +0000

    rutas

--- sofiapp/src/app/app-routing.module.ts
+++ sofiapp/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GaleriaComponent } from './secciones/galeria/galeria.component';
+import { ContactoComponent } from './secciones/contacto/contacto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'galeria', component: GaleriaComponent },
+  { path: 'contacto', component: ContactoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
